4c964f21b14be480d2a624d45cf389c30bfd3070
sC0der <102285034+SGc0der@users.noreply.github.com>
2023-06-17 22:25:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function redirect1() {
+    window.location = "object1.html";
+}
+function redirect2() {
+    window.location = "object2.html";
+}
+function redirect3() {
+    window.location = "object3.html";
+}
+function redirect4() {
+    window.location = "object4.html";
+}
+function redirect5() {
+    window.location = "object5.html";
+}
---
+++ object3.js
@@ -0,0 +1,35 @@
+function setup() {
+    canvas = createCanvas(640, 400);
+    canvas.center();
+}
+function preload() {
+    img = loadImage("study.jpeg");
+    cocossd = ml5.objectDetector("cocossd", modelLoaded);
+    for(i = 0; i < objects.length; i++) {
+        stroke("red");
+        fill("red");
+        percent = Math.floor(objects[i].confidence * 100) + "%";
+        text(objects[i].label + " " + percent, objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+}
+function draw() {
+    image(img, 0, 0, 640, 400);
+}
+status_ = "";
+function modelLoaded() {
+    console.log("model loaded");
+    cocossd.detect(img, gotResults);
+    status_ = true;
+}
+objects = [];
+function gotResults(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+    document.getElementById("status2").innerHTML = "Model detected " + objects.length + " objects";
+    document.getElementById("status").innerHTML = "Status: Objects Detected";
+}
